9f30fce5dacdf5b78c9969374aa4aa6962a4f9d7
hungdran8032 <nguyenhung20032308@gmail.com>
2024-06-12 13:09:58 +0700

    Init commit

---
+++ src/main/java/com/hutech/BaiTapGiuaKy/BaiTapGiuaKyApplication.java
@@ -0,0 +1,13 @@
+package com.hutech.BaiTapGiuaKy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaiTapGiuaKyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaiTapGiuaKyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hutech/BaiTapGiuaKy/BaiTapGiuaKyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hutech.BaiTapGiuaKy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaiTapGiuaKyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
